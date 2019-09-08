3d247556a6176dbe9c2fa346036383d5bf3aecd1
Sanjay Shahi <sanjay.shahi243@gmail.com>
2019-09-08 16:21:38 +0545

    Video upload added in media folder

--- WebApp/views.py
+++ WebApp/views.py
@@ -797,17 +797,17 @@ def chapterpagebuilder(request, course, chapter):
 @csrf_exempt
 def save_file(request):
     if request.method == "POST":
-        # count = request.POST['count']
         chapterID = request.POST['chapterID']
         courseID = request.POST['courseID']
+        media_type = request.POST['type']
         path = ''
         # for x in range(int(count)):
         if request.FILES['file-0']:
-            image = request.FILES['file-0']
-            if (image.size/1024) > 500:
-                return JsonResponse(data = {"message":"File size exceeds 2MB"}, status=500)
+            media = request.FILES['file-0']
+            if media_type == 'pic':
+                if (media.size/1024) > 2048:
+                    return JsonResponse(data = {"message":"File size exceeds 2MB"}, status=500)
             path = settings.MEDIA_ROOT
-            image
             # following is commented because filesystemstorage auto create directories if not exist
             # if not os.path.exists(os.path.join(path, 'chapterBuilder')):
             #     os.makedirs(os.path.join(path, 'chapterBuilder'))
@@ -816,7 +816,7 @@ def save_file(request):
             # if not os.path.exists(path+'chapterBuilder/'+courseID+'/'+chapterID):
             #     os.makedirs(os.path.join(path, 'chapterBuilder/'+courseID+'/'+chapterID))    
             fs = FileSystemStorage(location=path + '/chapterBuilder/' + courseID + '/' + chapterID)
-            filename = fs.save(image.name, image)
+            filename = fs.save(media.name, media)
         return JsonResponse(data={"message": "success"})
 
 
