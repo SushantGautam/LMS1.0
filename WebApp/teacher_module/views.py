2b096d26677f02cf1bd6d678a9c9f50c56e3dafb
Sabina Bhattarai <sabina.bhattrai2015@gmail.com>
2019-10-01 16:07:49 +0545

    student forum error corrected, reply count error corrected in nodegroup page

--- WebApp/teacher_module/views.py
+++ WebApp/teacher_module/views.py
@@ -1104,8 +1104,8 @@ class NodeGroupView(ListView):
             try:
                 thread = Thread.objects.filter(
                     topic=topic.pk).order_by('pub_date')[0]
-                reply_count = Thread.objects.filter(topic=topic.pk).aggregate(
-                    Sum('reply_count'))['reply_count__sum']
+                reply_count = Post.objects.filter(thread=thread.pk).count()
+
             except:
                 thread = None
             latest_threads.append([topic, thread, reply_count])
