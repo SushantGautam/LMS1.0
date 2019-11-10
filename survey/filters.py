923cc3aed83e4b3323d093c420a8e1b2dea70d8b
Sujil Devkota <sujil@nsdevil.com>
2019-11-10 13:25:30 +0545

    #36 Added Start date and end date filter amend

--- survey/filters.py
+++ survey/filters.py
@@ -5,9 +5,9 @@ from survey.models import SurveyInfo
 
 
 class SurveyFilter(FilterSet):
-    End_Date_lte = django_filters.DateTimeFilter(field_name="End_Date", lookup_expr='lte')
+    End_Date_lte = django_filters.DateTimeFilter(field_name="End_Date", lookup_expr='gte')
     Start_Date_lte = django_filters.DateTimeFilter(field_name="Start_Date", lookup_expr='lte')
 
     class Meta:
         model = SurveyInfo
-        fields = ['End_Date_lte','Start_Date_lte']
+        fields = ['End_Date_gte','Start_Date_lte']
