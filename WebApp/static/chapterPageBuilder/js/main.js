fc77f6dcf7a083f82fcff099826d80f3aa6d6fcb
sanjay.shahi243 <sanjay.shahi243@gmail.com>
2019-12-30 13:42:24 +0545

    open to previous page in builder

--- WebApp/static/chapterPageBuilder/js/main.js
+++ WebApp/static/chapterPageBuilder/js/main.js
@@ -1843,7 +1843,14 @@ $(document).ready(function () {
     setslider()
 
     $('.tabs-to-click > ul > li:first').remove()
-    changePage('1');
+
+    if(localStorage.getItem(`chapter_${chapterID}_currentPage`) && localStorage.getItem(`chapter_${chapterID}_currentPage`) <= data.numberofpages && localStorage.getItem(`chapter_${chapterID}_currentPage`) > 0){
+        window.firstload = false
+        changePage(localStorage.getItem(`chapter_${chapterID}_currentPage`));
+    }else{
+        changePage('1');
+    }
+
 
     // Button Form Submit
     $('#btn-submit').on('click', function () {
@@ -2555,6 +2562,7 @@ function changePage(page_number){
         }
         display(data)
     }
+    localStorage.setItem(`chapter_${chapterID}_currentPage`, window.currentPage);
 }
 
 // Media File deletion
