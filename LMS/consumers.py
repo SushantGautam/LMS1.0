17544d8a37edf4bb13637b2736f0f7ffd2162f35
Sushant Gautam <susant.gautam@gmail.com>
2020-01-06 23:42:45 +0545

    fix for filename

--- LMS/consumers.py
+++ LMS/consumers.py
@@ -19,28 +19,35 @@ def ws_message(message, group_id):
 def ws_disconnect(message, group_id):
     Group(group_id).discard(message.reply_channel)
 
+
 from datetime import datetime
 from django.conf import settings
 import os
 import json
 
+
 def storeChat(message, room_name):
     path = settings.MEDIA_ROOT
     if not os.path.exists(os.path.join(path, 'chatlog')):
         os.makedirs(os.path.join(path, 'chatlog'))
-    if not os.path.exists(os.path.join(path, 'chatlog/'+room_name)):
-        os.makedirs(os.path.join(path, 'chatlog/'+room_name))
+    if not os.path.exists(os.path.join(path, 'chatlog/' + room_name)):
+        os.makedirs(os.path.join(path, 'chatlog/' + room_name))
     data = json.loads(message.content['text'])
-    currenttime = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
-    if data['chat_message']:
-        with open(path + '/chatlog/' + room_name + '/' + currenttime + '.txt', 'w') as outfile:
-            chat_story = {
-                "sender_id":data['sender_id'],
-                "sender_name": data['sender_name'],
-                "sender_icon": data['sender_icon'],
-                "chat_message": data['chat_message'],
-                "date": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            }
-            json.dump(chat_story, outfile, indent=4)
+    currenttime = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')[:-4]
+    pathfile = path + '\\chatlog\\' + room_name + '\\' + currenttime + '.txt'
+    try:
+        if data['chat_message']:
+            with open(pathfile, 'w+') as outfile:
+                chat_story = {
+                    "sender_id": data['sender_id'],
+                    "sender_name": data['sender_name'],
+                    "sender_icon": data['sender_icon'],
+                    "chat_message": data['chat_message'],
+                    "date": datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+                }
+                json.dump(chat_story, outfile, indent=4)
+    except:
+        pass
+
     else:
         return
