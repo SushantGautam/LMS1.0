42ef858c6cfded61b0d04ac20b69bc17fe17ed27
Sushant Gautam <susant.gautam@gmail.com>
2020-02-14 10:38:02 +0545

    Progress for quiz and add new quiz fix

--- quiz/views.py
+++ quiz/views.py
@@ -1051,7 +1051,7 @@ class QuizSAQChoosePrevious(UpdateView):
 
 
 def FilterMarkingForTeachers(request, Quiz_Id):
-    filtered = Sitting.objects.filter(user__Center_Code=request.user.Center_Code, quiz=Quiz_Id, complete=True)
+    filtered = Sitting.objects.filter(user__Center_Code=request.user.Center_Code, quiz=Quiz_Id)
     quiz = Quiz.objects.get(id=Quiz_Id)
 
     return render(request, 'quiz/sitting_filter.html', {'sitting_list': filtered, 'quiz': quiz})
