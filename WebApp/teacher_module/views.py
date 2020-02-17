b451a8852bdaa8844cfda0c39931faf3147c6a9b
Sanjay Shahi <sanjay.shahi243@gmail.com>
2020-02-17 16:30:38 +0545

    Teacher student courselist filter issue fix

--- WebApp/teacher_module/views.py
+++ WebApp/teacher_module/views.py
@@ -177,6 +177,8 @@ class MyCourseListView(ListView):
             self.request.GET,
             queryset=courses
         ).qs
+        filtered_qs = filtered_qs.filter(Course_Code__in=context['object_list'].values_list('pk'))
+
         paginator = Paginator(filtered_qs, 8)
         page = self.request.GET.get('page')
         try:
