a05b5e4d06e88fc43f953aa8dbc101eb38d33fa3
Sanjay Shahi <sanjay.shahi243@gmail.com>
2020-03-31 17:17:41 +0545

    assignmentinfo_detail.html assignment filter

--- WebApp/student_module/views.py
+++ WebApp/student_module/views.py
@@ -317,7 +317,8 @@ class ChapterInfoDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['assignments'] = AssignmentInfo.objects.filter(
-            Chapter_Code=self.kwargs.get('pk'), Use_Flag=True)
+            Chapter_Code=self.kwargs.get('pk'), Use_Flag=True, Assignment_Start__lte=datetime_now.date(),
+            Assignment_Deadline__gte=datetime_now.date())
         context['post_quizes'] = Quiz.objects.filter(
             chapter_code=self.kwargs.get('pk'), draft=False, post_test=True)
         context['pre_quizes'] = Quiz.objects.filter(
