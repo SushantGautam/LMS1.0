2cfa22f3b7fbbf113a58a239193717ec7680c33f
Sanjay Shahi <sanjay.shahi243@gmail.com>
2020-05-04 21:26:06 +0545

    # Builder Quiz create teachers url added. # ubl error #49

--- WebApp/static/chapterPageBuilder/js/newmain.js
+++ WebApp/static/chapterPageBuilder/js/newmain.js
@@ -2569,30 +2569,28 @@ $(document).ready(function () {
         });
     });
 
+    $('#quiz_create_link').click(function (e) {
+        $('#iframeholder iframe').on('load', function () {
+            var iframe = $('#iframeholder iframe').contents();
+            $('#iframeholder iframe').contents().find("#quiz_form_ajax").on('click', '#quiz_submit_button', function () {
+                setTimeout(() => {
+                    modalcloseFunction()
+                }, 1000)
+            });
+        });
+    })
 
+    $('#survey_create_link').click(function (e) {
+        $('#iframeholder iframe').on('load', function () {
+            var iframe = $('#iframeholder iframe').contents();
+            $('#iframeholder iframe').contents().find("#survey_form_ajax").on('click', '#survey_submit_button', function () {
+                setTimeout(() => {
+                    modalcloseFunction()
+                }, 1000)
+            });
+        });
+    })
 });
-$('#quiz_create_link').click(function (e) {
-    $('#iframeholder iframe').on('load', function () {
-        var iframe = $('#iframeholder iframe').contents();
-        $('#iframeholder iframe').contents().find("#quiz_form_ajax").on('click', '#quiz_submit_button', function () {
-            setTimeout(() => {
-                modalcloseFunction()
-            }, 1500)
-        });
-    });
-
-})
-$('#survey_create_link').click(function (e) {
-    $('#iframeholder iframe').on('load', function () {
-        var iframe = $('#iframeholder iframe').contents();
-        $('#iframeholder iframe').contents().find("#survey_form_ajax").on('click', '#survey_submit_button', function () {
-            setTimeout(() => {
-                modalcloseFunction()
-            }, 1500)
-        });
-    });
-
-})
 
 function modalcloseFunction() {
     $('#closeiframebtn').click();
