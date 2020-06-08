b6768ad940488a4e415beaddff614a4770c22494
Sanjay Shahi <sanjay.shahi243@gmail.com>
2020-06-08 13:03:54 +0545

    # Quiz and Exam complete status issue in chapter and course detail

--- WebApp/templatetags/model_template_relation.py
+++ WebApp/templatetags/model_template_relation.py
@@ -34,3 +34,22 @@ def getTeacherStatusoOfAssignment(obj, user):
         'assg_status': assg_status,
         'total_students': completed + incomplete,
     }
+
+@register.simple_tag
+def getUserQuizStatus(obj, user):
+    userSittings = obj.filter(user__id=user.id)
+    if userSittings.exists():
+        isExist = True
+        for userSitting in userSittings:
+            if userSitting.complete:
+                isComplete = True
+                break
+            else:
+                isComplete = False
+    else:
+        isExist = False
+        isComplete = None
+    return {
+        'isExist': isExist,
+        'isComplete': isComplete
+    }
