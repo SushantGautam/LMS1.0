fa0ae0b04cd332f35520c7217aa9877e8338ab1a
Sushant Gautam <susant.gautam@gmail.com>
2020-06-30 15:10:37 +0545

    added center logo  to theme and models

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -16,7 +16,7 @@ class CenterInfoAdminForm(forms.ModelForm):
 
 class CenterInfoAdmin(admin.ModelAdmin):
     form = CenterInfoAdminForm
-    list_display = ['Center_Name', 'Center_Address', 'Use_Flag', 'Register_DateTime', 'Register_Agent']
+    list_display = ['Center_Name', 'Center_Logo', 'Center_Address', 'Use_Flag', 'Register_DateTime', 'Register_Agent']
 
 
 admin.site.register(CenterInfo, CenterInfoAdmin)
