a8891f7cfc90bb8da81ff23764aef353d54dc32c
suomynonAnonymous <juneisbackagain@gmail.com>
2020-08-23 18:03:10 +0545

    survey_plot

--- survey/views.py
+++ survey/views.py
@@ -681,26 +681,3 @@ def deleteSurvey(request):
             obj = SurveyInfo.objects.get(pk=request.POST['objectpk']).delete()
     return redirect('surveyinfo_list')
 
-
-def export_pdf(request):
-    # Instantiate ExportConfig and ExportManager
-    export_config = ExportConfig()
-    export_manager = ExportManager()
-
-    # Fetch the curr directory of the template
-    curr_dir = os.path.dirname(os.path.abspath(__file__))
-
-    template_path = os.path.join(curr_dir, 'templates/survey/xyz.html')
-
-    # Set all the configurations
-
-    export_config['templateFilePath'] = template_path
-    export_config['type'] = 'pdf'
-    export_config['templateFormat'] = 'A4'
-    export_config['asyncCapture'] = True
-
-    # Export your first PDF
-
-    # export_manager.export(export_config, unzip=True)
-
-    return JsonResponse((export_config['templateFilePath'],export_config['type'],export_config['templateFormat'],export_config['asyncCapture'],export_manager.export(export_config, unzip=True)), safe=False)
