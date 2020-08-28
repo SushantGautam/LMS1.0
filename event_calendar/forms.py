9c1bdd9a6cbd8ec3b6a8a705c6bc1bc7b94b26af
suomynonAnonymous <juneisbackagain@gmail.com>
2020-08-28 21:04:14 +0545

    update_view, Participation_type, Update_Modal

--- event_calendar/forms.py
+++ event_calendar/forms.py
@@ -6,7 +6,7 @@ from event_calendar.models import CalendarEvent
 class CalendarEventForm(ModelForm):
     class Meta:
         model = CalendarEvent
-        fields = ['title', 'event_type', 'date_start', 'date_end', 'description', 'participants', 'is_all_day']
+        fields = ['title', 'event_type', 'date_start', 'date_end', 'participation_type', 'description', 'participants', 'is_all_day']
 
 
 class CalendarEventUpdateForm(ModelForm):
