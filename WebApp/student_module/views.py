1c3c9fbd5b3b01dd3e33320ec78ff1ac20f33ad1
Sujil Devkota <sujil@nsdevil.com>
2020-09-18 19:55:10 +0545

    Function reused in coursedetail view

--- WebApp/student_module/views.py
+++ WebApp/student_module/views.py
@@ -361,20 +361,7 @@ class CourseInfoDetailView(CourseAuthMxnCls, StudentCourseAuthMxnCls, DetailView
                                                     Groups__in=student_groups,
                                                     Course_Group__in=course_groups)
 
-        chapters = ChapterInfo.objects.filter(Course_Code__pk=self.kwargs.get('pk'), Use_Flag=True)
-        active_chapters = []
-        for chapter in chapters:
-            if not SessionMapInfo.objects.filter(content_type= ContentType.objects.get_for_model(chapter),
-                                             object_id= chapter.id,
-                                             Session_Code__in = assigned_session).exists():
-                active_chapters.append(chapter)
-            elif SessionMapInfo.objects.filter(content_type= ContentType.objects.get_for_model(chapter),
-                                               object_id= chapter.id,
-                                               Start_Date__lte=datetime_now,
-                                               End_Date__gte=datetime_now,
-                                               Session_Code__in = assigned_session).exists():
-                active_chapters.append(chapter)
-        context['chapters'] = active_chapters
+        context['chapters'] = student_active_chapters(CourseInfo.objects.filter(pk=self.kwargs.get('pk')), assigned_session)
 
         # context['chapters'] = ChapterInfo.objects.filter(Course_Code=self.kwargs.get('pk'), Use_Flag=True,
         #                                                  ).filter(
