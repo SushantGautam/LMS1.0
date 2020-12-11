b62197af2c1be8778591c2d1657ef693e66e2717
Sanjay Shahi <sanjay.shahi243@gmail.com>
2020-12-11 09:39:53 +0545

    # quiz issue with empty question list and no query parameter

--- quiz/models.py
+++ quiz/models.py
@@ -836,7 +836,7 @@ class Sitting(models.Model):
         """
 
         if not question_index:
-            first, _ = self.question_list.split(',', 1) if self.question_list != '' else None
+            first, _ = self.question_list.split(',', 1) if self.question_list != '' else self.question_order.split(',', 1)
         else:
             first = self.question_order.split(',')[question_index-1]
         question_id = int(first)
