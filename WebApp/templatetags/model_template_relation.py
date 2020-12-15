ed42524274bcde77b822925ff408a303ed20dd86
Techno Guy <juneisbackagain@gmail.com>
2020-12-15 20:33:37 +0545

    Assignment_list,Chapter_Create&Assignment_Create_Fixed

--- WebApp/templatetags/model_template_relation.py
+++ WebApp/templatetags/model_template_relation.py
@@ -2,6 +2,7 @@
 
 from django import template
 from django.db.models import Sum
+from django.utils import timezone
 
 from WebApp.models import MemberInfo, AssignAnswerInfo
 
@@ -66,6 +67,12 @@ def getUser(userdata):
         return MemberInfo.objects.get(username=userdata)
     return userdata
 
+@register.simple_tag
+def getUserFullName(userdata):
+    if MemberInfo.objects.filter(username=userdata).exists():
+        return MemberInfo.objects.get(username=userdata).get_full_name()
+    return '--'
+
 
 @register.simple_tag
 def getAssignmentsScore(assignmentObj, userObj):
@@ -94,3 +101,30 @@ def getAssignmentAnswer(assignmentObj, userObj, questionObj):
 @register.filter
 def subtract(value, arg):
     return value - arg
+
+
+@register.simple_tag
+def isAssignmentActive(assignmentObj):
+    inningmaps = assignmentObj.assignment_sessionmaps.all()
+    isanswerable = False
+    now = timezone.now()
+    for inningmap in inningmaps:
+        if inningmap.Start_Date <= now and inningmap.End_Date >= now:
+            isanswerable = True
+            break
+    return isanswerable
+
+
+@register.simple_tag
+def getSessionMap(object, model_name, sessions=None):
+    if model_name == 'assignmentinfo':
+        return object.assignment_sessionmaps.filter(
+            Session_Code=sessions) if sessions else object.assignment_sessionmaps.all
+    elif model_name == 'chapterinfo':
+        return object.chapter_sessionmaps.filter(
+            Session_Code=sessions) if sessions else object.chapter_sessionmaps.all
+
+@register.simple_tag
+def getQuizQuestionIndex(sitting, questionID):
+    if str(questionID) in sitting.question_order.split(','):
+        return sitting.question_order.split(',').index(str(questionID)) + 1
