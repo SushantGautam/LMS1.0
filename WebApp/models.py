6fc6609490712abe9adeed2800015760250d1801
Sujil Devkota <Sujil@nsdevil.com>
2021-02-24 16:12:14 +0545

    Foreign key on_delete case update

--- WebApp/models.py
+++ WebApp/models.py
@@ -72,7 +72,7 @@ class DepartmentInfo(models.Model):
 
     Center_Code = ForeignKey(
         'CenterInfo',
-        related_name="departmentinfos", on_delete=models.SET_NULL, null=True
+        related_name="departmentinfos", on_delete=models.CASCADE, null=True
     )
 
     class Meta:
@@ -160,7 +160,7 @@ class MemberInfo(AbstractUser):
     Is_CenterAdmin = models.BooleanField(default=False)
     Is_Parent = models.BooleanField(default=False)
     Member_Gender = models.CharField(max_length=1, choices=Gender_Choices, default='F')
-    Member_Department = models.ForeignKey('DepartmentInfo', on_delete=models.DO_NOTHING, blank=True, null=True)
+    Member_Department = models.ForeignKey('DepartmentInfo', on_delete=models.SET_NULL, blank=True, null=True)
     Member_Position = models.CharField(max_length=30, choices=Position_Choices, blank=True, null=True)
     New_Flag = models.BooleanField(default=False)
 
